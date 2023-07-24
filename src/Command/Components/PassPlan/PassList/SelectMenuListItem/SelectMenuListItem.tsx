e9678d80560a5912475b6e1b0caf7a5169e3972a
Frances Morrison <francesrocket@Rocket-FRANCES.M>
2023-07-24 15:16:56 -0400

    chore: spacing adjustments to SelectMenuListItem

--- src/Command/Components/PassPlan/PassList/SelectMenuListItem/SelectMenuListItem.tsx
+++ src/Command/Components/PassPlan/PassList/SelectMenuListItem/SelectMenuListItem.tsx
@@ -5,38 +5,48 @@ import {
   RuxTableRow,
 } from "@astrouxds/react";
 import { useState } from "react";
-import "./SelectMenuListItem.css"
+import "./SelectMenuListItem.css";
 
 type PropTypes = {
   stepNumber: number;
 };
 
 const SelectMenuListItem = ({ stepNumber }: PropTypes) => {
-    const [xyz, setXyz] = useState<string>('yes')
-    const [receiveTelemetry, setReceiveTelemetry] = useState<string>('yes')
+  const [xyz, setXyz] = useState<string>("yes");
+  const [receiveTelemetry, setReceiveTelemetry] = useState<string>("yes");
   return (
     <>
       <RuxTableRow>
-        <RuxTableCell>{stepNumber}</RuxTableCell>
         <RuxTableCell>
-            <div className="pass_select-wrapper">
-            <RuxSelect label="Is 'XYZ' Selected?" value={xyz} onRuxchange={(e) => setXyz(e.target.value as string)}>
-            <RuxOption label="yes" value="yes">
-              Yes
-            </RuxOption>
-            <RuxOption label="no" value="no">
-              No
-            </RuxOption>
-          </RuxSelect>
-          <RuxSelect label="Are you receiving telemetry?" value={receiveTelemetry} onRuxchange={(e) => setReceiveTelemetry(e.target.value as string)}>
-            <RuxOption label="yes" value="yes">
-              Yes
-            </RuxOption>
-            <RuxOption label="no" value="no">
-              No
-            </RuxOption>
-          </RuxSelect>
-            </div>
+          <div className="pass_number-wrapper">{stepNumber}</div>
+        </RuxTableCell>
+        <RuxTableCell>
+          <div className="pass_select-wrapper">
+            <RuxSelect
+              label="Is 'XYZ' Selected?"
+              value={xyz}
+              onRuxchange={(e) => setXyz(e.target.value as string)}
+            >
+              <RuxOption label="yes" value="yes">
+                Yes
+              </RuxOption>
+              <RuxOption label="no" value="no">
+                No
+              </RuxOption>
+            </RuxSelect>
+            <RuxSelect
+              label="Are you receiving telemetry?"
+              value={receiveTelemetry}
+              onRuxchange={(e) => setReceiveTelemetry(e.target.value as string)}
+            >
+              <RuxOption label="yes" value="yes">
+                Yes
+              </RuxOption>
+              <RuxOption label="no" value="no">
+                No
+              </RuxOption>
+            </RuxSelect>
+          </div>
         </RuxTableCell>
       </RuxTableRow>
     </>
