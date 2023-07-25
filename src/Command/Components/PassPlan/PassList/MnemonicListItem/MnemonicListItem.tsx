25e711efa450c04a9adce5e2d7cfbe8000d94f7e
Frances Morrison <francesrocket@Rocket-FRANCES.M>
2023-07-25 15:07:24 -0400

    feat: adding watcher text and icon based on value of watched property on mnemonics object

--- src/Command/Components/PassPlan/PassList/MnemonicListItem/MnemonicListItem.tsx
+++ src/Command/Components/PassPlan/PassList/MnemonicListItem/MnemonicListItem.tsx
@@ -1,43 +1,48 @@
 import {
-    RuxCheckbox,
-    RuxTableCell,
-    RuxTableRow,
-  } from "@astrouxds/react";
-  import { generateMnemonics } from "@astrouxds/mock-data";
+  RuxCheckbox,
+  RuxIcon,
+  RuxTableCell,
+  RuxTableRow,
+} from "@astrouxds/react";
+import { generateMnemonics } from "@astrouxds/mock-data";
 import MnemonicPopUp from "../../../Watcher/MnemonicPopUp";
-  
-  type PropTypes = {
-    stepNumber: number;
-    mnemonicRowAmount: number;
-  };
 
+type PropTypes = {
+  stepNumber: number;
+  mnemonicRowAmount: number;
+};
 
+const MnemonicListItem = ({ stepNumber, mnemonicRowAmount }: PropTypes) => {
+  const mnemonicsData = generateMnemonics(mnemonicRowAmount, {});
   
-  const MnemonicListItem = ({ stepNumber, mnemonicRowAmount }: PropTypes) => {
-    const mnemonicsData = generateMnemonics(mnemonicRowAmount, {});
-    console.log(mnemonicsData)
-    return (
-      <>
-        {mnemonicsData.map(
-            (data) => {
-            return (
-                <RuxTableRow key={data.id}>
-                    <RuxTableCell>
-                        <div className="pass_number-wrapper">{stepNumber}</div>
-                    </RuxTableCell>
-                    <RuxTableCell>
-                        <div className="pass_mnemonic-wrapper">
-                            <RuxCheckbox />
-                            Verify{'\u00A0'}<MnemonicPopUp triggerValue={data.mnemonicId} data={data} />{'\u00A0 = \u00A0'}{data.currentValue}
-                        </div>
-                    </RuxTableCell>
-                </RuxTableRow>
-            );
-            }
-        )}
-      </>
-    );
-  };
-  
-  export default MnemonicListItem;
-  
+  return (
+    <>
+      {mnemonicsData.map((data) => {
+        return (
+          <RuxTableRow key={data.id}>
+            <RuxTableCell>
+              <div className="pass_number-wrapper">{stepNumber}</div>
+            </RuxTableCell>
+            <RuxTableCell>
+              <div className="pass_mnemonic-wrapper">
+                <RuxCheckbox />
+                {"Verify\u00A0"}
+                <MnemonicPopUp triggerValue={data.mnemonicId} data={data} />
+                {"\u00A0 = \u00A0"}
+                {data.currentValue}
+                {data.watched && (
+                  <div className="pass_mnemonic-watching">
+                    <RuxIcon icon="visibility" size="extra-small" />
+                    <i>Watcing</i>
+                  </div>
+                )}
+              </div>
+            </RuxTableCell>
+          </RuxTableRow>
+        );
+      })}
+    </>
+  );
+};
+
+export default MnemonicListItem;
