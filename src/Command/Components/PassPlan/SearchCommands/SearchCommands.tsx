04171db4ca9cacc9e917db1c31191f4e7245ee00
Kiley Mitti <kiley.mitti@rocketcom.com>
2023-07-27 14:11:24 -0400

    add recents and highlighting

--- src/Command/Components/PassPlan/SearchCommands/SearchCommands.tsx
+++ src/Command/Components/PassPlan/SearchCommands/SearchCommands.tsx
@@ -3,22 +3,23 @@ import {
   RuxInput,
   RuxMenu,
   RuxMenuItem,
+  RuxMenuItemDivider,
   RuxPopUp,
 } from "@astrouxds/react";
-import { Dispatch, SetStateAction, useState, useRef, useEffect } from "react";
+import { Dispatch, SetStateAction, useState } from "react";
 import "./SearchCommands.css";
 
 type PropTypes = {
-  commands: { commandString: string; description: string; commandId: number }[];
+  commands: CommandType[];
   setCommand: Dispatch<SetStateAction<object>>;
   command: object;
   addToPassQueue: any;
   pass: string;
 };
 
-type Command = {
+type CommandType = {
   commandId?: number;
-  command?: string;
+  commandString?: string;
   description?: string;
 };
 
@@ -29,9 +30,11 @@ const SearchCommands = ({
   pass,
 }: PropTypes) => {
   const [inputValue, setInputValue] = useState<string>("");
-  const [currentCommand, setCurrentCommand] = useState<Command | null>(null);
+  const [currentCommand, setCurrentCommand] = useState<CommandType | null>(
+    null
+  );
+  const [recentCommands, setRecentCommands] = useState<CommandType[]>([]);
   const isDisabled = pass === "Pre-Pass";
-  const searchPopup = useRef() as React.RefObject<HTMLRuxPopUpElement>;
 
   const filteredCommands = commands.filter((command) =>
     `${command.commandString}, (${command.description})`
@@ -39,13 +42,15 @@ const SearchCommands = ({
       .includes(inputValue.toLowerCase())
   );
 
-  useEffect(() => {
-    if (filteredCommands.length === 1) {
-      setCurrentCommand(filteredCommands[0]);
-    } else {
-      setCurrentCommand(null);
-    }
-  }, [filteredCommands]);
+  if (filteredCommands.length === 1 && !currentCommand) {
+    setCurrentCommand(filteredCommands[0]);
+  }
+
+  const filteredRecentCommands = recentCommands.filter((command) =>
+    `${command.commandString}, (${command.description})`
+      .toLowerCase()
+      .includes(inputValue.toLowerCase())
+  );
 
   //send command and reset inputs
   const sendCommand = (current: object) => {
@@ -53,39 +58,22 @@ const SearchCommands = ({
     addToPassQueue(current);
     setCurrentCommand(null);
     setInputValue("");
+
+    //deal with recentCommands
+    setRecentCommands((prevCommands) => {
+      if (prevCommands.length >= 5) {
+        prevCommands.pop();
+      }
+      return [current, ...prevCommands];
+    });
   };
 
   return (
     <>
-      <RuxPopUp placement="top-start">
-        <div className="history-popup">
-          <span>Recent Commands:</span>
-          <ul>
-            <li>80000</li>
-            <li>80010</li>
-            <li>Memory Dump 4</li>
-            <li>QPR Command 3</li>
-            <li>Satellite Command</li>
-          </ul>
-          <span>Quick Response Procedures:</span>
-          <ul>
-            <li>QRP Command</li>
-          </ul>
-          <span>All Commands:</span>
-          <ul>
-            {commands.map((item, index) => (
-              <li key={index}>
-                {item.commandString}, <i>({item.description})</i>
-              </li>
-            ))}
-          </ul>
-        </div>
-      </RuxPopUp>
       <RuxPopUp
         className="commands_input-pop-up"
         placement="top-start"
         closeOnSelect={true}
-        ref={searchPopup}
       >
         <span slot="trigger">
           <RuxButton
@@ -112,7 +100,7 @@ const SearchCommands = ({
             className="commands_input-menu"
             onRuxmenuselected={(e) => {
               const command = commands.find(
-                (command) => command.commandId.toString() === e.detail.value
+                (command) => command.commandId!.toString() === e.detail.value
               );
               setCurrentCommand(command || null);
               setInputValue(
@@ -120,11 +108,34 @@ const SearchCommands = ({
               );
             }}
           >
+            {filteredRecentCommands.length > 0 ? (
+              <>
+                <h4 className="menu-title">Recent Commands</h4>
+                {filteredRecentCommands.map((item, index) => (
+                  <RuxMenuItem
+                    selected={
+                      !!currentCommand &&
+                      currentCommand.commandId === item.commandId
+                    }
+                    key={index}
+                    value={item.commandId!.toString()}
+                  >
+                    {item.commandString}, <i>({item.description})</i>
+                  </RuxMenuItem>
+                ))}
+                <RuxMenuItemDivider />
+              </>
+            ) : null}
+
+            <h4 className="menu-title">Commands</h4>
             {filteredCommands.map((item, index) => (
               <RuxMenuItem
-                selected={filteredCommands.length === 1}
+                selected={
+                  !!currentCommand &&
+                  currentCommand.commandId === item.commandId
+                }
                 key={index}
-                value={item.commandId.toString()}
+                value={item.commandId!.toString()}
               >
                 {item.commandString}, <i>({item.description})</i>
               </RuxMenuItem>
