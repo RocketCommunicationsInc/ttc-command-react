42b45d6fe7bea22c80fa00b366d8bd67c54006ce
Frances Morrison <francesrocket@Rocket-FRANCES.M>
2023-07-27 13:36:51 -0400

    fix: getting the rux-button to work in the executable rux-tree-node (this is an interim fix, once the actual web component is in it should be taken out)

--- src/Command/Components/PassPlan/PassList/ExecutableListItem/ExecutableListItem.tsx
+++ src/Command/Components/PassPlan/PassList/ExecutableListItem/ExecutableListItem.tsx
@@ -1,5 +1,5 @@
 import { RuxButton, RuxProgress, RuxIcon, RuxTreeNode } from "@astrouxds/react";
-import { useMemo } from "react";
+import { useMemo, useRef } from "react";
 import MnemonicListItem from "../MnemonicListItem/MnemonicListItem";
 
 type PropTypes = {
@@ -11,6 +11,8 @@ const ExecutableListItem = ({ stepNumber }: PropTypes) => {
     return Math.random() * (max - min) + min;
   };
 
+  const progressBar = useRef<HTMLRuxProgressElement>(null);
+
   const mnemonicItems = useMemo(() => {
     const randomNumber = getRandomInt(2, 5);
     const numberArray = Array.from({ length: randomNumber }, (_, i) => i + 1);
@@ -18,6 +20,7 @@ const ExecutableListItem = ({ stepNumber }: PropTypes) => {
     return Array.from({ length: randomNumber }, (_, index) => {
       return (
         <MnemonicListItem
+        key={index}
           stepNumber={`${stepNumber}.${numberArray[index]}`}
           slotNode={true}
         />
@@ -25,17 +28,28 @@ const ExecutableListItem = ({ stepNumber }: PropTypes) => {
     });
   }, [stepNumber]);
 
+  const handleExecuteButtonClick = () => {
+    let value = 0;
+    const progressInterval = setInterval(() => {
+        if (value > 100) clearInterval(progressInterval)
+        progressBar.current!.value = value +1
+        value = value +1
+    },10)
+  }
+
   return (
+    <div className="pass_executable-parent">
+    <RuxButton className="pass_execute-button" iconOnly icon="play-arrow" onClick={handleExecuteButtonClick}></RuxButton>
     <RuxTreeNode expanded>
       <div slot="prefix" className="pass_number-wrapper">
         {stepNumber}
       </div>
       <div className="pass_executable-wrapper">
-        <RuxButton iconOnly icon="play-arrow" />
+        <RuxButton iconOnly icon="play-arrow" onClick={handleExecuteButtonClick}></RuxButton>
         <div className="pass_executable-progress-wrapper">
           <div className="pass_command-name">Command Name Placeholder</div>
           <div className="pass_progress-time">
-            <RuxProgress hideLabel />
+            <RuxProgress ref={progressBar} hideLabel />
             <RuxIcon icon="schedule" size="extra-small" />
             00:00:25
           </div>
@@ -43,6 +57,7 @@ const ExecutableListItem = ({ stepNumber }: PropTypes) => {
       </div>
       {mnemonicItems}
     </RuxTreeNode>
+    </div>
   );
 };
 
