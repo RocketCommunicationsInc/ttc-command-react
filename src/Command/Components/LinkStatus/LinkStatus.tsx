18311e142b7abf9db36b1d412a1dc52f259e0277
Nic Dominguez <NicDominguez@users.noreply.github.com>
2023-08-01 14:54:28 -0700

    remove unnecessary hooks

--- src/Command/Components/LinkStatus/LinkStatus.tsx
+++ src/Command/Components/LinkStatus/LinkStatus.tsx
@@ -9,38 +9,31 @@ import {
 import { getRandomInt } from "utils";
 import "./LinkStatus.css";
 import { Status } from "@astrouxds/mock-data";
-import { useEffect, useMemo, useState } from "react";
+
+const statusValuesArr: Status[] = [
+  "off",
+  "caution",
+  "critical",
+  "normal",
+  "serious",
+  "standby",
+];
+
+const getRandomStatus: () => Status = () =>
+  statusValuesArr[Math.floor(Math.random() * statusValuesArr.length)];
+
+const status1 = getRandomStatus();
+const status2 = getRandomStatus();
+const status3 = getRandomStatus();
+
+const int1 = getRandomInt(-100, -1);
+const int2 = getRandomInt(-100, -1);
+const int3 = getRandomInt(50, 90);
+const int4 = getRandomInt(10000, 99999);
+const int5 = getRandomInt(1000, 9999);
+const int6 = getRandomInt(0, 5);
 
 const LinkStatus = () => {
-  const [status1, setStatus1] = useState<Status>("off");
-  const [status2, setStatus2] = useState<Status>("standby");
-  const [status3, setStatus3] = useState<Status>("normal");
-
-  useEffect(() => {
-    const statusValuesArr = [
-      "off",
-      "caution",
-      "critical",
-      "normal",
-      "serious",
-      "standby",
-    ];
-
-    const randomStatus = Math.floor(Math.random() * statusValuesArr.length);
-    const randomStatus2 = Math.floor(Math.random() * statusValuesArr.length);
-    const randomStatus3 = Math.floor(Math.random() * statusValuesArr.length);
-    setStatus1(statusValuesArr[randomStatus] as Status);
-    setStatus2(statusValuesArr[randomStatus2] as Status);
-    setStatus3(statusValuesArr[randomStatus3] as Status);
-  }, []);
-
-  const int1 = useMemo(() => getRandomInt(-100, -1), []);
-  const int2 = useMemo(() => getRandomInt(-100, -1), []);
-  const int3 = useMemo(() => getRandomInt(50, 90), []);
-  const int4 = useMemo(() => getRandomInt(10000, 99999), []);
-  const int5 = useMemo(() => getRandomInt(1000, 9999), []);
-  const int6 = useMemo(() => getRandomInt(0, 5), []);
-
   return (
     <RuxContainer className="link-status">
       <div slot="header">Link Status</div>
