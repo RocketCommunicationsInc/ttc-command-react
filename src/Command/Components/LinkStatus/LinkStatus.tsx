b559b20d5eccb975d72741d71757eabce0193333
Maria Paterno <97068764+Paterma@users.noreply.github.com>
2023-08-01 15:19:07 -0600

    update Link Status with random values and status

--- src/Command/Components/LinkStatus/LinkStatus.tsx
+++ src/Command/Components/LinkStatus/LinkStatus.tsx
@@ -6,9 +6,34 @@ import {
   RuxTableCell,
   RuxTableRow,
 } from "@astrouxds/react";
+import { getRandomInt } from "utils";
 import "./LinkStatus.css";
+import { Status } from "@astrouxds/mock-data";
+import { useEffect, useState } from "react";
 
 const LinkStatus = () => {
+  const [status1, setStatus1] = useState<Status>("off");
+  const [status2, setStatus2] = useState<Status>("standby");
+  const [status3, setStatus3] = useState<Status>("normal");
+
+  useEffect(() => {
+    const statusValuesArr = [
+      "off",
+      "caution",
+      "critical",
+      "normal",
+      "serious",
+      "standby",
+    ];
+
+    const randomStatus = Math.floor(Math.random() * statusValuesArr.length);
+    const randomStatus2 = Math.floor(Math.random() * statusValuesArr.length);
+    const randomStatus3 = Math.floor(Math.random() * statusValuesArr.length);
+    setStatus1(statusValuesArr[randomStatus] as Status);
+    setStatus2(statusValuesArr[randomStatus2] as Status);
+    setStatus3(statusValuesArr[randomStatus3] as Status);
+  }, []);
+
   return (
     <RuxContainer className="link-status">
       <div slot="header">Link Status</div>
@@ -17,30 +42,34 @@ const LinkStatus = () => {
           <RuxTableRow>
             <RuxTableCell>
               <span>
-                <RuxStatus status="serious" /> Lock
-                <span className="total">-99</span>
+                <RuxStatus status={status1} /> Lock
+                <span className="total">{getRandomInt(-100, -1)}</span>
               </span>
             </RuxTableCell>
           </RuxTableRow>
           <RuxTableRow>
-            <RuxTableCell>Signal Strength: -72.64</RuxTableCell>
+            <RuxTableCell>
+              Signal Strength: {getRandomInt(-100, -1)}
+            </RuxTableCell>
           </RuxTableRow>
           <RuxTableRow>
             <RuxTableCell>
               <span>
-                <RuxStatus status="serious" />
-                Telemetry<span className="total">99.0</span>
+                <RuxStatus status={status2} />
+                Telemetry<span className="total">{getRandomInt(50, 90)}</span>
               </span>
             </RuxTableCell>
           </RuxTableRow>
           <RuxTableRow>
-            <RuxTableCell>Total Frame Count: 49336</RuxTableCell>
+            <RuxTableCell>
+              Total Frame Count: {getRandomInt(10000, 99999)}
+            </RuxTableCell>
           </RuxTableRow>
           <RuxTableRow>
             <RuxTableCell>
               <span>
-                <RuxStatus status="serious" />
-                VCC <span className="total">7,290</span>
+                <RuxStatus status={status3} />
+                VCC <span className="total">{getRandomInt(1000, 9999)}</span>
               </span>
             </RuxTableCell>
           </RuxTableRow>
